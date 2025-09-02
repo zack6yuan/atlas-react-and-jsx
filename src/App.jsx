ce1c2f7fd76b08372d48359aa26e24e8786f328f
zack6yuan <zack6yuan@gmail.com>
2025-09-02 13:21:44 -0500

    first task complete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
+// Render the App component to the root element
 function App() {
   return <div className="app">Hello Atlas</div>;
 }
-
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+// Main file
 import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-// Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />)
