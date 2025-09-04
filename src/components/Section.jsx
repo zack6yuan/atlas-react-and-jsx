3fd9bf3506db993cd1b2d5c8c2c3c2f7b5fd385f
zack6yuan <zack6yuan@gmail.com>
2025-09-04 09:51:17 -0500

    lowercase.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,7 +6,7 @@ function Section() {
             React is an open source front-end JavaScript library developed and maintained by Meta that helps developers build single-page applications.
             The building blocks of React are broken down into functional components, which makes code maintainable and reusable.
             With a strong ecosystem containing various tools and libraries, React simplifies the process of building dynamic and interactive web applications.
-            <h1>Benefits of React</h1>
+            <h1>Benefits of react</h1>
             <ul>
                 <li>Component-based architecture that makes components maintainable and scaleable</li>
                 <li>Declarative syntax that makes code easier to understand</li>
