43c475abc8c6dcd138715ec27bea78a67a60ce2b
zack6yuan <zack6yuan@gmail.com>
2025-09-04 14:38:27 -0500

    Making changes to the app.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ function App() {
   return (
     <div className="app">
         <Header />
-        <Section title="What is react?" ></Section>
+        <Section title="What is react?" >
+            React is an open source front-end JavaScript library developed and maintained by Meta that helps developers build single-page applications.
+            The building blocks of React are broken down into functional components, which makes code maintainable and reusable.
+            With a strong ecosystem containing various tools and libraries, React simplifies the process of building dynamic and interactive web applications.
+        </Section>
         <HelpfulResource title="Helpful Resources" />
         <AboutMe />
         <Footer />
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 
-function Section({ title }) {
+function Section({ title, children }) {
     return (
         <div className="about-section">
             <h1>{ title }</h1>
-            React is an open source front-end JavaScript library developed and maintained by Meta that helps developers build single-page applications.
-            The building blocks of React are broken down into functional components, which makes code maintainable and reusable.
-            With a strong ecosystem containing various tools and libraries, React simplifies the process of building dynamic and interactive web applications.
-            <h1>Benefits of react</h1>
-            <ul>
-                <li>Component-based architecture that makes components maintainable and scaleable</li>
-                <li>Declarative syntax that makes code easier to understand</li>
-                <li>Efficient rendering to the DOM (Document Object Model)</li>
-                <li>Simplifies implementing dynamic content and interactive UI elements</li>
-                <li>Strong ecosystem of tools and libraries to pair with the core functionality of React</li>
-            </ul>
+            { children }
         </div>
     );
 }
