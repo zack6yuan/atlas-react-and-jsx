f89e8323ee1b3e5794120c9a102e89707dd0547f
zack6yuan <zack6yuan@gmail.com>
2025-09-04 14:20:13 -0500

    dynamic time and image loading is working.

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -7,15 +7,32 @@ import night from "../assets/night.svg";
 
 // Greeting component
 function Greeting() {
-    const date = new Date().getHours();
-    
+    const time = new Date().getHours();
+
+    let image;
+    let greeting;
+
+    if (time >= 6 && time < 12) {
+        image = day;
+        greeting = "Good Morning!"
+    } else if (time >= 12 && time < 17) {
+        image = day;
+        greeting = "Good Afternoon!";
+    } else if (time >= 17 && time < 21) {
+        image = evening;
+        greeting = "Good Evening!";
+    } else {
+        image = night;
+        greeting = "Good Night!"
+    }
+
     return (
         <div>
             <h1 className="greeting">
-                <img src={day} alt="day" />
-                Good Morning!
+                <img src={ image }></img>
+                <p>{ greeting }</p>
             </h1>
         </div>
-    );
+    )
 }
 export default Greeting;
