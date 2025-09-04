ae67e802f3d9a30fd0a958a954b66f81b603a122
zack6yuan <zack6yuan@gmail.com>
2025-09-04 11:46:37 -0500

    updating the about me and footer section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Section from "./components/Section";
 import HelpfulResource from "./components/HelpfulResource";
+import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
 
 // Render the App component to the root element
 function App() {
@@ -9,6 +11,8 @@ function App() {
         <Header />
         <Section />
         <HelpfulResource />
+        <AboutMe />
+        <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import links from "../assets/links.json";
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>component</li>
+                <li>component</li>
+                <li>component</li>
+            </ul>
+            <p>
+                © 2025 Atlas School
+            </p>
+        </footer>
+    );
+}
+export default Footer;
