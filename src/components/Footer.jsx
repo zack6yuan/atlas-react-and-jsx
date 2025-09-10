4d564fa974b11388519c80e25387050bea4dd049
zack6yuan <zack6yuan@gmail.com>
2025-09-10 13:08:16 -0500

    footer links are working.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,25 @@
 import links from "../assets/links.json";
 
 function Footer() {
+    // get the current year
     const year = new Date().getFullYear();
+
+    // define specific links from imported JSON
+    const atlasLink = links[0].link;
+    const reactLink = links[1].link;
+    const netlifyLink = links[2].link;
     return (
         <footer>
             <ul>
-                <li>{links[0].label}</li>
-                <li>{links[1].label}</li>
-                <li>{links[2].label}</li>
+                <li>
+                    <a href={atlasLink}>{links[0].label}</a>
+                </li>
+                <li>
+                    <a href={reactLink}>{links[1].label}</a>
+                </li>
+                <li>
+                    <a href={netlifyLink}>{links[2].label}</a>
+                </li>
             </ul>
             <p>
                 © {year} Atlas School
